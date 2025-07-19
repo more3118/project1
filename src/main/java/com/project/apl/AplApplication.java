014646fb27edcfe528c057a8f38a935674752ebf
Shreyas <more3118@gmail.com>
2025-07-19 09:50:26 -0500

    Initial Commit

---
+++ src/main/java/com/project/apl/AplApplication.java
@@ -0,0 +1,13 @@
+package com.project.apl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplApplication.class, args);
+	}
+
+}
